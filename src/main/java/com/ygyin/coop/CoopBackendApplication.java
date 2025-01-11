1a546301db1ad916d5800580cb523549333d378d
YGYin <ygyin0307@gmail.com>
2025-01-11 21:40:01 +0800

    Init

---
+++ src/main/java/com/ygyin/coop/CoopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ygyin.coop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoopBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoopBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ygyin/coop/CoopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ygyin.coop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoopBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
