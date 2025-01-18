93539503af7743e5c903880c9058881b28ba8d6e
YGYin <ygyin0307@gmail.com>
2025-01-18 16:23:04 +0800

    Test controller

--- src/main/java/com/ygyin/coop/controller/BasicController.java
+++ src/main/java/com/ygyin/coop/controller/BasicController.java
@@ -14,31 +14,29 @@
  * limitations under the License.
  */
 
-package com.ygyin.coop.demos.web;
+package com.ygyin.coop.controller;
 
+import com.ygyin.coop.common.BaseResponse;
+import com.ygyin.coop.common.ResUtils;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author <a href="mailto:chenxilzx1@gmail.com">theonefx</a>
  */
 @Controller
-public class PathVariableController {
+public class BasicController {
 
-    // http://127.0.0.1:8080/user/123/roles/222
-    @RequestMapping(value = "/user/{userId}/roles/{roleId}", method = RequestMethod.GET)
+
+    @RequestMapping("/hello/test")
     @ResponseBody
-    public String getLogin(@PathVariable("userId") String userId, @PathVariable("roleId") String roleId) {
-        return "User Id : " + userId + " Role Id : " + roleId;
+    public BaseResponse<String> helloTest(@RequestParam(name = "name", defaultValue = "unknown user") String name) {
+        System.out.println(1 / 0);
+        // return new BaseResponse<>(200, "Hello " + name);
+        return ResUtils.success("Hello " + name);
     }
 
-    // http://127.0.0.1:8080/javabeat/somewords
-    @RequestMapping(value = "/javabeat/{regexp1:[a-z-]+}", method = RequestMethod.GET)
-    @ResponseBody
-    public String getRegExp(@PathVariable("regexp1") String regexp1) {
-        return "URI Part : " + regexp1;
-    }
 }
