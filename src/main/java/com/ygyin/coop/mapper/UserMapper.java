73b75bb16be936be1842ea3f2a86385fed7b86ad
YGYin <ygyin0307@gmail.com>
2025-01-18 16:27:52 +0800

    Mapper for User

---
+++ src/main/java/com/ygyin/coop/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.ygyin.coop.mapper;
+
+import com.ygyin.coop.model.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author yg
+* @description 针对表【user(用户)】的数据库操作Mapper
+* @createDate 2025-01-16 16:10:30
+* @Entity generator.domain.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
