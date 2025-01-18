da2021ffaffa189e211f80157c06d2a676ef8095
YGYin <ygyin0307@gmail.com>
2025-01-18 16:24:18 +0800

    Utils for throwing business exception

---
+++ src/main/java/com/ygyin/coop/exception/ThrowUtils.java
@@ -0,0 +1,37 @@
+package com.ygyin.coop.exception;
+
+public class ThrowUtils {
+
+    /**
+     * 满足条件抛出异常
+     *
+     * @param condition
+     * @param runtimeException
+     */
+    public static void throwIf(boolean condition, RuntimeException runtimeException) {
+        if (condition) {
+            throw runtimeException;
+        }
+    }
+
+    /**
+     * 满足条件，根据错误码抛业务异常
+     *
+     * @param condition
+     * @param errorCode
+     */
+    public static void throwIf(boolean condition, ErrorCode errorCode) {
+        throwIf(condition, new BusinessException(errorCode));
+    }
+
+    /**
+     * 满足条件，根据错误码抛业务异常，自定义异常信息
+     *
+     * @param condition
+     * @param errorCode
+     * @param message
+     */
+    public static void throwIf(boolean condition, ErrorCode errorCode, String message) {
+        throwIf(condition, new BusinessException(errorCode, message));
+    }
+}
