5fa135da10358b3561b1257d23314acbeb54dc09
YGYin <ygyin0307@gmail.com>
2025-01-18 16:27:22 +0800

    Add mapper scan

--- src/main/java/com/ygyin/coop/CoopBackendApplication.java
+++ src/main/java/com/ygyin/coop/CoopBackendApplication.java
@@ -1,9 +1,13 @@
 package com.ygyin.coop;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@MapperScan("com.ygyin.coop.mapper")
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class CoopBackendApplication {
 
     public static void main(String[] args) {
