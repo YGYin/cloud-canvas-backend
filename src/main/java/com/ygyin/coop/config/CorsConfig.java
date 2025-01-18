bcc68e26274242d1664359f55e02684db63f135a
YGYin <ygyin0307@gmail.com>
2025-01-18 16:26:06 +0800

    Global cors config

---
+++ src/main/java/com/ygyin/coop/config/CorsConfig.java
@@ -0,0 +1,20 @@
+package com.ygyin.coop.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // 用于覆盖全部请求
+        registry.addMapping("/**")
+                .allowCredentials(true) // 允许 cookie
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .exposedHeaders("*");
+    }
+}
