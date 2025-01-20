de494d10e8a2a622e327a831d8b77ccad0909453
YGYin <ygyin0307@gmail.com>
2025-01-20 15:45:32 +0800

    User constant for states and roles

---
+++ src/main/java/com/ygyin/coop/constant/UserConstant.java
@@ -0,0 +1,23 @@
+package com.ygyin.coop.constant;
+
+public interface UserConstant {
+
+    // 接口内默认为 final static 常量
+    /**
+     * 用户登录状态 key
+     */
+    String USER_LOGIN_STATE = "user_login";
+
+    // 权限管理
+
+    /**
+     * 默认权限
+     */
+    String DEFAULT_ROLE = "user";
+
+    /**
+     * 管理员权限
+     */
+    String ADMIN_ROLE = "admin";
+
+}
