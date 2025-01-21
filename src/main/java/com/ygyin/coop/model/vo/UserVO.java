8cdf2d2c773bfa90bcd2f64d7a456c7db5134675
YGYin <ygyin0307@gmail.com>
2025-01-21 17:59:28 +0800

    User VO class

---
+++ src/main/java/com/ygyin/coop/model/vo/UserVO.java
@@ -0,0 +1,52 @@
+package com.ygyin.coop.model.vo;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 脱敏后的用户信息视图包装类，用于返回给前端
+ */
+
+@Data
+public class UserVO implements Serializable {
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 账号
+     */
+    private String userAccount;
+
+
+    /**
+     * 用户昵称
+     */
+    private String userName;
+
+    /**
+     * 用户头像
+     */
+    private String userAvatar;
+
+    /**
+     * 用户简介
+     */
+    private String userProfile;
+
+    /**
+     * 用户角色：user/admin
+     */
+    private String userRole;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    private static final long serialVersionUID = 1L;
+}
+
