67188e07d741e1e9481ef9b96d60e49d1c5f7377
YGYin <ygyin0307@gmail.com>
2025-02-08 12:17:51 +0800

    Add image mapper

---
+++ src/main/java/com/ygyin/coop/mapper/ImageMapper.java
@@ -0,0 +1,18 @@
+package com.ygyin.coop.mapper;
+
+import com.ygyin.coop.model.entity.Image;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author yg
+* @description 针对表【image(图片)】的数据库操作Mapper
+* @createDate 2025-01-29 17:07:54
+* @Entity com.ygyin.coop.model.entity.Image
+*/
+public interface ImageMapper extends BaseMapper<Image> {
+
+}
+
+
+
+
