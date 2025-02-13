c96d0620ce4c6c714f774c2909ab55c606b4f93a
YGYin <ygyin0307@gmail.com>
2025-02-13 14:32:34 +0800

    Add Area mapper

---
+++ src/main/java/com/ygyin/coop/mapper/AreaMapper.java
@@ -0,0 +1,18 @@
+package com.ygyin.coop.mapper;
+
+import com.ygyin.coop.model.entity.Area;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author yg
+* @description 针对表【area(空间)】的数据库操作Mapper
+* @createDate 2025-02-11 11:41:31
+* @Entity com.ygyin.coop.model.entity.Area
+*/
+public interface AreaMapper extends BaseMapper<Area> {
+
+}
+
+
+
+
