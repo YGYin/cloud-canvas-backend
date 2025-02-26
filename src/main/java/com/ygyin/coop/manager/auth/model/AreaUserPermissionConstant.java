b7223a16214ed6f90fdfb1cb5f0ae23581f3eb96
YGYin <ygyin0307@gmail.com>
2025-02-26 18:11:12 +0800

    Identify the permission constant for area user

---
+++ src/main/java/com/ygyin/coop/manager/auth/model/AreaUserPermissionConstant.java
@@ -0,0 +1,31 @@
+package com.ygyin.coop.manager.auth.model;
+
+/**
+ * 空间成员权限定义 key 常量
+ */
+public interface AreaUserPermissionConstant {
+    /**
+     * 空间用户管理权限
+     */
+    String AREA_USER_MANAGE = "areaUser:manage";
+
+    /**
+     * 图片查看权限
+     */
+    String IMAGE_VIEW = "image:view";
+
+    /**
+     * 图片上传权限
+     */
+    String IMAGE_UPLOAD = "image:upload";
+
+    /**
+     * 图片编辑权限
+     */
+    String IMAGE_EDIT = "image:edit";
+
+    /**
+     * 图片删除权限
+     */
+    String IMAGE_DELETE = "image:delete";
+}
